5a347ebe4830a6d71c5175af90a8cf8b5958b0e7
Squidlider <aleksi.koskiaho@gmail.com>
2023-03-02 22:50:54 +0200

    fronttimuutokset ja uusi buildi

--- index.js
+++ index.js
@@ -23,33 +23,43 @@ app.use(
 
 let persons = []
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
 app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
     response.json(persons)
   })
 })
 
-app.get('/info', (req, res) => {
+app.get('/info', (req, response) => {
   Person.find({}).then((persons) => {
     const personCount = persons.length
-    res.send(`
+    response.send(`
       <p>Phonebook has info for ${personCount} people</p>
       <p>${new Date()}</p>
     `)
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find((person) => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  Person.findById(id)
+    .then((person) => {
+      response.json(person)
+    })
+    .catch((error) => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   // const alreadyAdded = persons.find((person) => person.name === body.name)
@@ -71,37 +81,45 @@ app.post('/api/persons', (request, response) => {
     number: body.number,
   })
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson)
-  })
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson)
+    })
+    .catch((error) => next(error))
 })
 
-app.put('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find((person) => person.id === id)
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
 
-  if (!person) {
-    return response.status(404).json({
-      error: 'person not found',
+  if (!body.number || !body.name) {
+    return response.status(400).json({
+      error: 'Name or number missing',
     })
   }
 
-  const body = request.body
-  const updatedPerson = {
-    ...person,
+  const person = {
+    name: body.name,
     number: body.number,
   }
 
-  persons = persons.map((person) => (person.id !== id ? person : updatedPerson))
-
-  response.json(updatedPerson)
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson)
+    })
+    .catch((error) => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter((person) => person.id !== id)
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  Person.findByIdAndRemove(id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
